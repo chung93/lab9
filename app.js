7d09c91043580ead473031b7c037fb22758c8c69
cienna <cchung93@my.bcit.ca>
2021-04-19 22:30:36 -0700

    I forgot to copy the string iteration part

--- app.js
+++ app.js
@@ -11,3 +11,6 @@ if (x == y) { "they match" } else { "no luck" }
 x = y
 if (x == y) { "they match" } else { "no luck" }
 "they match"
+for (let count = 0; count < a.length; count++);
+undefined
+//I don't know what I'm supposed to get? But I think I followed the syntax
